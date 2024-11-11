9a7f84508df295a1b70505d545bac76bf66d57b1
Anuprash Gautam <anuprashgautam@hotmail.com>
2024-11-11 22:20:24 +0530

    Added video 26 codes.

---
+++ 26 Video Boolean Type in SpEL/com.springcore.spel/Demo.java
@@ -0,0 +1,66 @@
+package com.springcore.spel;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Demo {
+	@Value("#{10}")
+	private int x;
+	
+	@Value("#{20+1}")
+	private int y;
+	
+	@Value("#{T(java.lang.Math).sqrt(16)}")
+	private double z;
+	
+	@Value("#{T(java.lang.Math).E}")
+	private double e;
+	
+	@Value("#{T(java.lang.Math).PI}")
+	private double pi;
+	
+	@Value("#{new java.lang.String('Anuprash Gautam')}")
+	private String name;
+	
+	
+	@Value("#{8>3?false:true}")
+	private boolean isActive;
+	
+
+	public double getE() {
+		return e;
+	}
+	public void setE(double e) {
+		this.e = e;
+	}
+	public double getPi() {
+		return pi;
+	}
+	public void setPi(double pi) {
+		this.pi = pi;
+	}
+	public double getZ() {
+		return z;
+	}
+	public void setZ(int z) {
+		this.z = z;
+	}
+	public int getX() {
+		return x;
+	}
+	public void setX(int x) {
+		this.x = x;
+	}
+	public int getY() {
+		return y;
+	}
+	public void setY(int y) {
+		this.y = y;
+	}
+	@Override
+	public String toString() {
+		return "Demo [x=" + x + ", y=" + y + ", z=" + z + ", e=" + e + ", pi=" + pi + ", name=" + name + ", isActive="
+				+ isActive + "]";
+	}
+}
